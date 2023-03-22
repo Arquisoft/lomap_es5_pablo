9a0c8418c62b868d4d2c3c392b341979db909666
Jose Emilio Labra Gayo <jelabra@gmail.com>
2023-03-22 17:18:16 +0100

    Removed api.ts from colectCoverageFrom

--- restapi/jest.config.ts
+++ restapi/jest.config.ts
@@ -4,5 +4,9 @@ export default {
         "^.+\\.tsx?$": "ts-jest"
     },
     collectCoverage: true,
-    collectCoverageFrom:["api.ts"]
+    collectCoverageFrom:[
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    ]
 }
